1c1fcb097b6c23b7a0b8d6a0700b0c7607c14301
fjpa654 <37986977+fjpa654@users.noreply.github.com>
2022-05-17 13:50:58 -0600

    Add files via upload

---
+++ ball.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+import random
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.color("white")
+        self.shape("circle")
+        self.shapesize(1, 1)
+        self.x_move = 10
+        self.y_move = 10
+
+    def move(self):
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.setposition(0, 0)
+        self.bounce_x()
